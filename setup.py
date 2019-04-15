b9794af267a7b23ebbe6827a188527cab7d9ec8f
Agustín <agustin.bouillet@gmail.com>
2019-04-15 17:02:45 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 from distutils.core import setup
 
 setup(
-  name='Validador de CUIT',
-  version='1.0.0',
-  py_modules=['cuit',],
-  author='Agustin Bouillet',
-  author_email='agustin.bouillet@gmail.com',
-  url='http://www.bouillet.com.ar/',
-  description='Validador de CUIT',
+    name='Validador de CUIT',
+    version='0.1',
+    packages = ['python-cuit'],
+    author='Agustin Bouillet',
+    author_email='agustin.bouillet@gmail.com',
+    url='http://www.bouillet.com.ar/',
+    description='Validador de CUIT',
+    keywords = ['cuil', 'cuit', 'Argentina'],
 )
