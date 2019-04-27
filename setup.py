3d683576dd1164865147e187c68cfb6c4b730d8d
Agustín Bouillet <agustinbouillet@Agustins-MacBook-Pro.local>
2019-04-27 03:35:04 -0300

    estructura

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from distutils.core import setup
 
 setup(
-    name='cuit',
+    name='python-cuit',
     version='0.1',
-    packages = ['cuit'],
+    py_modules = ['cuit'],
     author='Agustin Bouillet',
     author_email='agustin.bouillet@gmail.com',
     url='https://github.com/agustinbouillet/python-cuit',
